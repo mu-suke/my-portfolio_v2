bf543540f0bdbcf25f6b628ec2dcc07276c8fddd
mu-suke <chelsea_08ym@yahoo.co.jp>
2021-09-12 19:33:41 +0900

    add styles

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from '@chakra-ui/layout'
+import Head from 'next/head'
+import React from 'react'
+
+export const Layout = ({
+  title,
+  children,
+}: {
+  title?: string
+  children: any
+}) => {
+  return (
+    <div
+      style={{
+        backgroundImage: 'url(./bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Head>
+        <title>{title ?? 'mu-suke'}</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Container maxW='container.sm'>{children}</Container>
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from '@chakra-ui/react'
+import React from 'react'
+import { Layout } from '../components/Layout'
 
 const colors = {
   brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
   },
 }
 
