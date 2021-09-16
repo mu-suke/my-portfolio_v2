b20dcbd474be3cf2bc6209020de514866c3ba80b
mu-suke <chelsea_08ym@yahoo.co.jp>
2021-09-16 23:52:26 +0900

    add abou page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,77 @@
-import { Button } from '@chakra-ui/react'
-import { motion } from 'framer-motion'
+import { StarIcon } from '@chakra-ui/icons'
+import { Badge, Box, Image } from '@chakra-ui/react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { BackButton } from './common/BackButton'
+import { Motion } from './Layout/Motion'
 
 const About = () => {
+  const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of modern home with pool",
+    beds: 3,
+    baths: 2,
+    title: "Modern home in city center in the heart of historic Los Angeles",
+    formattedPrice: "$1,900.00",
+    reviewCount: 34,
+    rating: 4,
+  }
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ ease: "easeOut", duration: 1 }}
-    >
-      <Link to='/'>
-        <Button color='gray.50' colorScheme='gray.50' variant='outline' width='100%'>
-          Back
-        </Button>
-      </Link>
-    </motion.div>
+    <Motion>
+
+      <Box boxShadow='0rem 0.75rem 2.625rem 0rem rgb(40 39 48 / 52%)' borderRadius="lg" overflow="hidden">
+        <Image src={property.imageUrl} alt={property.imageAlt} />
+
+        <Box p="6" backgroundColor='rgba(40, 39, 48, 0.922)'>
+          <BackButton />
+          <Box d="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              New
+            </Badge>
+            <Box
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              ml="2"
+            >
+              {property.beds} beds &bull; {property.baths} baths
+            </Box>
+          </Box>
+
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            {property.title}
+          </Box>
+
+          <Box>
+            {property.formattedPrice}
+            <Box as="span" color="gray.600" fontSize="sm">
+              / wk
+            </Box>
+          </Box>
+
+          <Box d="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <StarIcon
+                  key={i}
+                  color={i < property.rating ? "teal.500" : "gray.300"}
+                />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {property.reviewCount} reviews
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Motion>
   )
 }
 
